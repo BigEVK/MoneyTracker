939cbf89cce310f5e09b0698c4d66bcf99409049
Eric VanKirk <eric@ericvankirk.com>
2022-07-11 19:31:14 -0500

    read only

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,28 @@
 document.querySelector("button").addEventListener("click", money);
 
+// --- adding event to press 'enter' and update balance as substitution for above ----
+
+// const input = document.getElementById("balance");
+
+// input.addEventListener("keyup", function(e) {
+//    if (e.keycode === 13) {  
+//       alert("This works");  
+
+
+//    }
+// });
+// var input = document.getElementById("balance");
+// input.addEventListener("keyup", function(e) {
+//   if (e.keycode === 13) {
+//    //  event.preventDefault();
+//    //  document.getElementById("balance").click();
+//   }
+// });
+
+// ---- None of the above worked so I have added an 'Update Balance' button in its stead ---
+
+const ledgerIdCounter = 0;
+
 function money() {
    var date = document.getElementById("date").value;
    var desc = document.getElementById("desc").value;
@@ -25,11 +48,15 @@ function money() {
    else {
       const ledger = document.createElement("div");
       ledger.classList.add("lineItemOne");
+      ledger.setAttribute("data-ledger-id", ledgerIdCounter);
+      console.log(ledger);
 
       const ledgerOne = document.createElement("input");      
-      ledgerOne.classList.add("container");   
-      // ledgerOne.setAttribute("type", "date",);
-      ledgerOne.setAttribute("data-date", dateIdCounter);
+      ledgerOne.classList.add("container"); 
+       
+      // ---This sets the date container --- why is it not sized correctly?? --- 
+      ledgerOne.setAttribute("type", "date",);
+      // ledgerOne.setAttribute("data-date", dateIdCounter);
    //  --- working on this at the moment ----
       // const 
 
@@ -61,18 +88,38 @@ function money() {
 
 
       const ledgerFour = document.createElement("input");
-      ledgerFour.classList.add("container", "lineItemOne");
+      ledgerFour.classList.add("container");
       ledgerFour.placeholder = "amount"
       ledger.appendChild(ledgerFour);
 
-      const ledgerFive = document.createElement("button");
+      const ledgerFive = document.createElement("input");
       ledgerFive.classList.add("container");
-
-      ledgerFive.innerHTML = "Balance"
+      ledgerFive.setAttribute("readonly", "text");
+      // ledgerFive.innerHTML = "Balance"
       ledger.appendChild(ledgerFive);
 
-      dateIdCounter++;
+
+      // dateIdCounter++;
       document.querySelector("body").appendChild(ledger);
+      console.log(ledger);
+
+      let ledgerDataObj = ledgerIdCounter;
+
+      ledgerDataObj.id = ledgerIdCounter;
+      // ledger.push(ledgerDataObj);
+      ledgerIdCounter++;
    };
 
+   saveLedger();
+
 };
+
+function newBal() {
+
+
+}
+
+var saveLedger = function () {
+   localStorage.Storage.setItem("ledger", JSON.stringify(ledger));
+}
+
